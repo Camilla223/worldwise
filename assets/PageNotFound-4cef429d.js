c500dbe2fac0506e5f2fd2f8cc5fba40874eb455
Camilla223 <39516759+Camilla223@users.noreply.github.com>
2026-01-29 21:47:34 +0100

    Updates

---
+++ assets/PageNotFound-4cef429d.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-170af999.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
--- assets/PageNotFound-f472eaf5.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-bb1d1de7.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
